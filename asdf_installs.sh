bc9c1e7f3c89ab97af52a1c7cdc6e1445da1501b
otosky <olivertosky@gmail.com>
2022-01-15 19:57:16 -0500

    move asdf plugins to separate script

---
+++ asdf_installs.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# neovim
+if asdf list | grep -q '0.6.1'; then
+  echo 'Neovim 0.6.1 already installed via asdf'
+else
+  asdf plugin add neovim
+  asdf install neovim O.6.1
+  asdf global neovim 0.6.1
+fi
+
+# python
+asdf plugin add python
+asdf install python 3.7.10
+asdf install python 3.8.8
+asdf install python 3.9.9
+asdf global python 3.9.9
+
+asdf plugin-add poetry https://github.com/asdf-community/asdf-poetry.git
+asdf install poetry 1.1.12
+asdf global poetry 1.1.12
+
+asdf plugin add java
+asdf install java adoptopenjdk-8.0.312+7
+asdf global java adoptopenjdk-8.0.312+7
+
+asdf plugin add golang
+
+# utilities
+asdf plugin-add terraform
+asdf install terraform 1.1.3
+
+asdf plugin-add yq
+
+asdf plugin add awscli
+asdf plugin-add kubectl
+

493f2a79275da055f3e7b32d785c602a83a3ce56
otosky <olivertosky@gmail.com>
2022-02-21 13:28:59 -0500

    add whichkey; TODO customize hints

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -100,7 +100,6 @@ return packer.startup(function(use)
 	use "lewis6991/gitsigns.nvim"
 	use("tpope/vim-fugitive")
 	use("tpope/vim-rhubarb")
-	-- use("airblade/vim-gitgutter")
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins

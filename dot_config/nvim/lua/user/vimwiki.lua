3777a881aa3a61b07654c88ba5c730086dc83712
otosky <olivertosky@gmail.com>
2022-07-09 20:31:23 -0400

    update zettel_date_format to include seconds & tz

--- dot_config/nvim/lua/user/vimwiki.lua
+++ dot_config/nvim/lua/user/vimwiki.lua
@@ -4,6 +4,7 @@ vim.g.vimwiki_list = {
 
 -- zettel
 vim.g.zettel_format = "%y%m%d-%H%M-%title"
+vim.g.zettel_date_format = "%Y-%m-%dT%H:%M:%S%z"
 
 local function save_title_to_register(register)
   local fname = vim.fn.expand('%:t')

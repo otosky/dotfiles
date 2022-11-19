1d7a7ae52b243bf52b1f1ae483e23a75124f8b53
otosky <olivertosky@gmail.com>
2022-11-19 00:25:25 -0500

    remove starship & add vim bindings

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,10 +1,11 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 if not test -e $HOME/.config/fish/functions/fisher.fish
   echo 'Installing fisher'
   curl -sL https://git.io/fisher | source 
 end
 
-starship init fish | source
+set -U fish_key_bindings fish_vi_key_bindings
+
+# interactive things
+status is-interactive || exit
+
+set -U fish_greeting

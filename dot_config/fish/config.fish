46386086fc8855a77cc02fae397099c2b355c27b
otosky <olivertosky@gmail.com>
2022-11-21 10:22:14 -0500

    editor var requires asdf to be sourced first

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -8,7 +8,6 @@ set -gx fish_key_bindings fish_vi_key_bindings
 set -gx fish_greeting
 set -gx POETRY_VIRTUALENVS_IN_PROJECT 'true'
 set -gx DIRENV_LOG_FORMAT ''
-set -gx EDITOR (which nvim)
 
 # interactive things
 status is-interactive || exit
@@ -16,3 +15,4 @@ status is-interactive || exit
 source ~/.asdf/asdf.fish
 direnv hook fish | source
 
+set -gx EDITOR (which nvim)

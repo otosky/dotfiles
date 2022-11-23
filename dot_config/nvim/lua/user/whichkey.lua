236ab79c3969a7a0510276d7439c1ca3783d0adc
otosky <olivertosky@gmail.com>
2022-11-23 16:21:26 -0500

    enable telescope-project plugin

--- dot_config/nvim/lua/user/whichkey.lua
+++ dot_config/nvim/lua/user/whichkey.lua
@@ -88,7 +88,7 @@ local mappings = {
 		"Find files",
 	},
 	["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
-	["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
+	["P"] = { "<cmd>lua require('telescope').extensions.project.project()<cr>", "Projects" },
 
 	p = {
 		name = "Packer",

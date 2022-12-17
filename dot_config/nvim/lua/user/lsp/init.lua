363a8eaa4b6e7d60473b9dc396a78447ebd90d02
Oliver Tosky <olivertosky@gmail.com>
2022-12-17 15:45:42 -0500

    remove lsp-installer and use Mason

--- dot_config/nvim/lua/user/lsp/init.lua
+++ dot_config/nvim/lua/user/lsp/init.lua
@@ -3,9 +3,39 @@ if not status_ok then
 	return
 end
 
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+})
+
+local servers = {
+	"sumneko_lua",
+	"rust_analyzer",
+	"pyright",
+	"tsserver",
+	"gopls",
+}
+
+require("mason-lspconfig").setup({
+	ensure_installed = servers,
+})
+
+local handler = require("user.lsp.handlers")
+handler.setup()
+
+for _, lsp in ipairs(servers) do
+	require("lspconfig")[lsp].setup({
+		on_attach = handler.on_attach,
+		capabilities = handler.capabilities,
+	})
+end
+
 require("user.lsp.keymaps")
-require("user.lsp.lsp-installer")
-require("user.lsp.handlers").setup()
 require("user.lsp.null-ls")
 require("user.lsp.dap")
 require("elixir").setup()
@@ -15,4 +45,4 @@ require("user.lsp.metals")
 
 -- dap clients
 require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
-require("dap-python").test_runner = 'pytest'
+require("dap-python").test_runner = "pytest"

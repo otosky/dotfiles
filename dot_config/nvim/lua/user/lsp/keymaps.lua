aa52f10485caefa274eeb2b71866ecd802ab789c
Oliver Tosky <olivertosky@gmail.com>
2022-12-16 23:17:21 -0500

    add keymap for lsp rename

--- dot_config/nvim/lua/user/lsp/keymaps.lua
+++ dot_config/nvim/lua/user/lsp/keymaps.lua
@@ -2,11 +2,11 @@ local api = vim.api
 local cmd = vim.cmd
 
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- lsp
@@ -15,6 +15,7 @@ map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
 map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
 map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
 map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+map("n", "<leader>rn", "<cmd>lua vim.buf.rename()<CR>")
 map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
 map("n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>')
 map("n", "gl", '<cmd>lua vim.diagnostic.open_float(0, {scope="line"})<CR>')

e184651d7c6b789a27b407802b6d792f2a0c8d05
Oliver Tosky <olivertosky@gmail.com>
2022-12-17 16:27:12 -0500

    clean up treesitter config

--- dot_config/nvim/lua/user/treesitter.lua
+++ dot_config/nvim/lua/user/treesitter.lua
@@ -4,15 +4,13 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = { "python", "lua", "elixir" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = { "" }, -- List of parsers to ignore installing
+	ensure_installed = { "python", "lua", "elixir" },
+	sync_install = false,
 	autopairs = {
 		enable = true,
 	},
 	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "" }, -- list of language that will be disabled
+		enable = true,
 		additional_vim_regex_highlighting = true,
 	},
 	indent = { enable = true, disable = { "yaml" } },

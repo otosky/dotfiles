631c86b177217bc61824568cd696ac803c841b65
Oliver Tosky <olivertosky@gmail.com>
2023-01-29 19:15:51 -0500

    add extra formatting sources

--- dot_config/nvim/lua/plugins/null-ls.lua
+++ dot_config/nvim/lua/plugins/null-ls.lua
@@ -6,10 +6,14 @@ return {
     local nls = require("null-ls")
     return {
       sources = {
-        -- nls.builtins.formatting.prettierd,
-        nls.builtins.formatting.stylua,
         nls.builtins.diagnostics.flake8,
-        nls.builtins.formatting.black,
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
+        nls.builtins.formatting.isort,
+        nls.builtins.formatting.mix,
+        nls.builtins.formatting.prettier,
+        nls.builtins.formatting.scalafmt,
+        nls.builtins.formatting.shfmt,
       },
     }
   end,

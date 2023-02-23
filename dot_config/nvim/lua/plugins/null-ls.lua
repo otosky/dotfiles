fd36ccb49ecc02a7577dcadab792258e36856c31
otosky <olivertosky@gmail.com>
2023-02-23 15:02:07 -0500

    remove flake8 for real

--- dot_config/nvim/lua/plugins/null-ls.lua
+++ dot_config/nvim/lua/plugins/null-ls.lua
@@ -6,7 +6,6 @@ return {
     local nls = require("null-ls")
     return {
       sources = {
-        nls.builtins.diagnostics.flake8.with({ extra_args = { "--max-line-length 120" } }),
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
         nls.builtins.formatting.isort,

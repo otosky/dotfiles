728ea0763fcb572e4a2e92c32d46617afadbc5ec
otosky <olivertosky@gmail.com>
2023-03-05 15:12:45 -0500

    don't autoupdate brew installs

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -8,6 +8,7 @@ set -gx fish_key_bindings fish_vi_key_bindings
 set -gx fish_greeting
 set -gx POETRY_VIRTUALENVS_IN_PROJECT 'true'
 set -gx DIRENV_LOG_FORMAT ''
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
 
 
 # interactive things

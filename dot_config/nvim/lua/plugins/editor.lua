0a5e3211cf2b3417a9031c71893a256130f7a63c
otosky <olivertosky@gmail.com>
2023-03-04 19:10:00 -0500

    add markdown-preview

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,16 +1,23 @@
 return {
-  { "mechatroner/rainbow_csv" },
-  { "ap/vim-css-color" },
+	{ "mechatroner/rainbow_csv" },
+	{ "ap/vim-css-color" },
 
-  -- languague specifics
-  {
-    "cfmeyers/dbt.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "rcarriga/nvim-notify",
-    },
-  },
-  { "mhanberg/elixir.nvim", dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim" } },
-  { "scalameta/nvim-metals", dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" } },
+	{
+		"iamcco/markdown-preview.nvim",
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+
+	-- languague specifics
+	{
+		"cfmeyers/dbt.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"rcarriga/nvim-notify",
+		},
+	},
+	{ "mhanberg/elixir.nvim", dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim" } },
+	{ "scalameta/nvim-metals", dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" } },
 }

b6ba43b5ef62fdfd1a78fedc031832dbb6bfd057
otosky <olivertosky@gmail.com>
2023-04-22 11:51:01 -0400

    fix: configure nvim-metals

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,23 +1,22 @@
 return {
-	{ "mechatroner/rainbow_csv" },
-	{ "ap/vim-css-color" },
+  { "mechatroner/rainbow_csv" },
+  { "ap/vim-css-color" },
 
-	{
-		"iamcco/markdown-preview.nvim",
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 
-	-- languague specifics
-	{
-		"cfmeyers/dbt.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-			"rcarriga/nvim-notify",
-		},
-	},
-	{ "mhanberg/elixir.nvim", dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim" } },
-	{ "scalameta/nvim-metals", dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" } },
+  -- languague specifics
+  {
+    "cfmeyers/dbt.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+  { "mhanberg/elixir.nvim", dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim" } },
 }
--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,46 @@
 return {
-  "neovim/nvim-lspconfig",
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "<leader>cr", false }
-    keys[#keys + 1] = { "<leader>cr", vim.lsp.buf.rename }
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "<leader>cr", false }
+      keys[#keys + 1] = { "<leader>cr", vim.lsp.buf.rename }
+    end,
+  },
+  {
+    "scalameta/nvim-metals",
+    dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
+    ft = { "scala", "sbt", "java" },
+    config = function()
+      local metals_config = require("metals").bare_config()
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local dap = require("dap")
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+          },
+        },
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test Target",
+          metals = {
+            runType = "testTarget",
+          },
+        },
+      }
+
+      metals_config.on_attach = function(client, bufnr)
+        require("metals").setup_dap()
+      end
+
+      require("metals").initialize_or_attach(metals_config)
+    end,
+  },
 }

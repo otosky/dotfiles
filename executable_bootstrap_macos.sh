67cc6f65ce8380817eaa4e7153b6adc7340e1e9f
Oliver Tosky <olivertosky@gmail.com>
2023-05-16 22:16:19 -0400

    v1 bootstrap macos script

---
+++ executable_bootstrap_macos.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# install homebrew
+if [ ! "$(command -v brew)" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# install kitty
+
+if [ ! "$(command -v kitty)" ]; then
+  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+fi
+
+brew install \
+  homebrew/cask-fonts/font-fontawesome \
+  koekeishiya/formulae/yabai \
+  koekeishiya/formulae/skhd \
+  git-delta \
+  ripgrep \
+  fd \
+  tree \
+  automake \
+  fish \
+  tmux \
+  jq \
+  xsv \
+  sops \
+  openssl readline sqlite3 xz zlib libffi \
+  noti \
+  websocat \
+  just
+
+# fzf
+if [ ! "$(command -v fzf)" ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf \
+  && ~/.fzf/install --key-bindings --completion --no-update-rc
+fi

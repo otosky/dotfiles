5f2da7edf8bb364a9e41ffa002d1323e58c7850a
Oliver Tosky <olivertosky@gmail.com>
2023-08-06 17:41:37 -0400

    add nvim-surround

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -72,6 +72,17 @@ return {
     end,
   },
 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+
   -- languague specifics
   {
     "cfmeyers/dbt.nvim",

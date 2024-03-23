51f64141788f7556af88213c76c4fe8f6dbbd985
Oliver Tosky <olivertosky@gmail.com>
2024-03-22 22:30:39 -0400

    add a snippet to generate a UUID4

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -3,6 +3,7 @@ return {
     "L3MON4D3/LuaSnip",
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" })
+      require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
     end,
   },
 }
